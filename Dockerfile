aa5ef6a19738e5faf3a00f145cad71c4001ffee3
theverygaming <theverygaming@users.noreply.github.com>
2024-10-07 13:02:39 +0200

    stuff

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,20 @@ ARG ALPINE_VERSION=3.20.3
 FROM alpine:${ALPINE_VERSION} AS build
 ARG ALPINE_VERSION
 WORKDIR /src
-COPY ./src /src
 
 RUN apk --no-cache add \
     curl
 
-ENV ALPINE_VERSION=${ALPINE_VERSION}
+# lmao there is probably a FAR better way of grabbing the linux kernel binary from alpine pkgs
+RUN apk add --no-cache linux-lts && mv /boot/vmlinuz-lts ./kernel
+
+RUN curl https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-${ALPINE_VERSION}-x86_64.tar.gz -o alpine-rootfs.tar.gz
+RUN mkdir alpine-rootfs && tar xf alpine-rootfs.tar.gz -C alpine-rootfs
+
+COPY ./src /src
+
 RUN ./alpine-build-root.sh
 
 FROM scratch
-#COPY --from=build /src/kernel /
+COPY --from=build /src/kernel /
 COPY --from=build /src/initramfs /
--- src/alpine-build-root.sh
+++ src/alpine-build-root.sh
@@ -1,31 +1,43 @@
 #!/bin/sh
 set -euo pipefail
 
-curl https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-${ALPINE_VERSION}-x86_64.tar.gz -o alpine-rootfs.tar.gz
-mkdir alpine-rootfs
-tar xf alpine-rootfs.tar.gz -C alpine-rootfs
 cd alpine-rootfs
+
 rm ./sbin/init
-cat > ./sbin/init <<EOF
+cat > ./init <<"EOF"
 #!/bin/sh
 mount -t devtmpfs dev /dev
 mount -t proc proc /proc
 mount -t sysfs sysfs /sys
+
+# reboot after 5s in case of a kernel panic
+echo 5 > /proc/sys/kernel/panic
+
 ip link set up dev lo
 
-exec /sbin/getty -n -l /bin/sh 115200 /dev/console
-poweroff -f
+EXEC_CMD=$(cat /proc/cmdline | sed -n 's/.*initscript_cmd=\("[^"]*"\|\S*\).*/\1/p' | sed 's/^"\(.*\)"$/\1/')
+
+echo "command to execute (from kernel params): '${EXEC_CMD}'" > /dev/console
+
+/bin/sh -c "${EXEC_CMD}" 2>&1 > /dev/console
+
+while :; do reboot -f; sleep 1; done
 EOF
 
-chmod +x ./sbin/init
+chmod +x ./init
 
 # temporarily copy the resolv.conf to the chroot folder for internet access
 cp /etc/resolv.conf ./etc/resolv.conf
 
 chroot . /bin/ash -l << EOF
 set -euo pipefail
-apk add --no-cache openssh openrc
-rc-update add sshd
+apk add --no-cache \
+    openssh \
+    util-linux-misc \
+    cryptsetup \
+    lvm2 \
+    e2fsprogs-extra \
+    dosfstools
 EOF
 
 # delete the resolv.conf again
@@ -34,12 +46,6 @@ rm ./etc/resolv.conf
 # build the initramfs image
 #find . -print0 | cpio --null --create --verbose --format=newc --owner root:root > ../initramfs
 #mkinitramfs -o ../initramfs -r ./ -v
-find . | sort | cpio --quiet --renumber-inodes -o -H newc > ../initramfs
+find . -print0 | cpio --null --create --verbose --format=newc --owner root:root > ../initramfs
 
 cd ..
-
-
-# lmao there is probably a FAR better way of grabbing the linux kernel binary from alpine pkgs
-#apk add --no-cache linux-lts
-#ls -lah /boot/vmlinuz-lts
-#mv /boot/vmlinuz-lts ./kernel
